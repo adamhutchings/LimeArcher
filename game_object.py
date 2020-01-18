3a145ed5a8f71e161c65a5a1b49ec449fff2d9a3
adamhutchings <adam.abahot@gmail.com>
2020-01-18 10:55:22 -0800

    Add basic controls

---
+++ game_object.py
@@ -0,0 +1,42 @@
+# For game objects LMAO
+
+import turtle
+
+class GameObject():
+
+	def __init__(self, shape, color, width, height, x, y):
+		self.t = turtle.Turtle()
+		self.t.speed(0)
+		self.t.penup()
+		self.t.color(color)
+		self.t.shape(shape)
+		self.t.shapesize(stretch_len = width, stretch_wid = height)
+		self.t.goto(x, y)
+
+		self.h = height
+		self.w = width
+
+	# Basic movement
+	def _up(self, amount):
+		self.t.sety(self.t.ycor() + amount)
+	def _down(self, amount):
+		self.t.sety(self.t.ycor() - amount)
+	def _left(self, amount):
+		self.t.setx(self.t.xcor() - amount)
+	def _right(self, amount):
+		self.t.setx(self.t.xcor() + amount)
+
+	def up(self):
+		self._up(20)
+	def down(self):
+		self._down(20)
+	def left(self):
+		self._left(20)
+	def right(self):
+		self._right(20)
+
+	def binds(self, wn, binds):
+		wn.onkeypress(self.up, binds[0])
+		wn.onkeypress(self.down, binds[1])
+		wn.onkeypress(self.left, binds[2])
+		wn.onkeypress(self.right, binds[3])
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle, bye, Terminator
 from _tkinter import TclError
 
+from game_object import GameObject
+
 # Window
 wn = Screen()
 wn.setup(height = 800, width = 1000)
@@ -11,6 +13,13 @@ wn.listen()
 
 # Goodbye
 wn.onkeypress(bye, 'Escape')
+
+# Player (testing)
+player1 = GameObject('circle', '#880000', 1, 1, -200, 0)
+player1.binds(wn, ['w', 's', 'a', 'd'])
+
+player2 = GameObject('square', '#008800', 1, 1, 200, 0)
+player2.binds(wn, ['Up', 'Down', 'Left', 'Right'])
 
 try:
 	while True:
