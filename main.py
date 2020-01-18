35086ee87ad0b113caa4b0ae9b2ad3e03cee7022
adamhutchings <adam.abahot@gmail.com>
2020-01-18 15:06:44 -0800

    Incorporate gravity and bouncing

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from turtle import Screen, Turtle, bye, Terminator
 from _tkinter import TclError
 
-from game_object import GameObject
+from game_object import GameObject, objs, Obstacle
+
+from time import sleep
+
+# Constants
+FRAMERATE = 30
 
 # Window
 wn = Screen()
@@ -11,18 +16,45 @@ wn.bgcolor('#220022')
 wn.tracer(0)
 wn.listen()
 
-# Goodbye
+# Goodbye to the game
 wn.onkeypress(bye, 'Escape')
 
-# Player (testing)
+# Players
 player1 = GameObject('circle', '#880000', 1, 1, -200, 0)
 player1.binds(wn, ['w', 's', 'a', 'd'])
 
 player2 = GameObject('square', '#008800', 1, 1, 200, 0)
 player2.binds(wn, ['Up', 'Down', 'Left', 'Right'])
 
+# Their lives
+player1.lives = 3
+player2.lives = 3
+
+# The floor
+floor = Obstacle(10, 'x', '#FFFFFF', 0, -250)
+
 try:
 	while True:
+
+		# Updating everything
 		wn.update()
+
+		# Gravity
+		for obj in objs:
+			obj.tick()
+
+			if obj.gravity:
+				obj.vec.affect_gravity(2, FRAMERATE)
+
+			if isinstance(obj, Obstacle):
+				obj.check_for_collisions()
+
+			for player in [player1, player2]:
+				if player.t.ycor() < -400:
+					player.lives -= 1
+
+		# One complete iteration
+		sleep(1/FRAMERATE)
+
 except (Terminator, TclError):
 	pass;
