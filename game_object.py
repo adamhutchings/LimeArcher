b24dca3f6fe47e83e424176d682d9689c907d316
adamhutchings <adam.abahot@gmail.com>
2020-01-20 13:41:46 -0800

    Convert tabs to spaces

--- game_object.py
+++ game_object.py
@@ -10,85 +10,85 @@ objs = []
 
 class GameObject():
 
-	def __init__(self, shape, color, width, height, x, y):
-		self.t = turtle.Turtle()
-		self.t.speed(0)
-		self.t.penup()
-		self.t.color(color)
-		self.t.shape(shape)
-		self.t.shapesize(stretch_len = width, stretch_wid = height)
-		self.t.goto(x, y)
+    def __init__(self, shape, color, width, height, x, y):
+        self.t = turtle.Turtle()
+        self.t.speed(0)
+        self.t.penup()
+        self.t.color(color)
+        self.t.shape(shape)
+        self.t.shapesize(stretch_len = width, stretch_wid = height)
+        self.t.goto(x, y)
 
-		self.h = height
-		self.w = width
-		self.gravity = True
+        self.h = height
+        self.w = width
+        self.gravity = True
 
-		self.vec = Vector(0, 0)
+        self.vec = Vector(0, 0)
 
-		objs.append(self)
+        objs.append(self)
 
-	# Basic movement
-	def _up(self, amount):
-		self.t.sety(self.t.ycor() + amount)
-	def _down(self, amount):
-		self.t.sety(self.t.ycor() - amount)
-	def _left(self, amount):
-		self.t.setx(self.t.xcor() - amount)
-	def _right(self, amount):
-		self.t.setx(self.t.xcor() + amount)
+    # Basic movement
+    def _up(self, amount):
+        self.t.sety(self.t.ycor() + amount)
+    def _down(self, amount):
+        self.t.sety(self.t.ycor() - amount)
+    def _left(self, amount):
+        self.t.setx(self.t.xcor() - amount)
+    def _right(self, amount):
+        self.t.setx(self.t.xcor() + amount)
 
-	# Actual player movements
-	def up(self):
-		self.vec.y = 4
-	def left(self):
-		self._left(20)
-	def right(self):
-		self._right(20)
+    # Actual player movements
+    def up(self):
+        self.vec.y = 4
+    def left(self):
+        self._left(20)
+    def right(self):
+        self._right(20)
 
-	def down(self):
+    def down(self):
 
-		# Going though every obstacle
-		# to see if it's in range
-		for obj in objs:
-			if isinstance(obj, Obstacle):
+        # Going though every obstacle
+        # to see if it's in range
+        for obj in objs:
+            if isinstance(obj, Obstacle):
 
-				# x-cor checking
-				avgWid = (self.w + obj.w)/2
-				if abs(obj.t.xcor() - self.t.xcor()) < avgWid:
+                # x-cor checking
+                avgWid = (self.w + obj.w)/2
+                if abs(obj.t.xcor() - self.t.xcor()) < avgWid:
 
-					# y-cor checking
-					yDiff = self.t.ycor() - obj.t.ycor()
-					if yDiff < 100 and yDiff > 0:
-						self.t.sety(obj.t.ycor()+((self.h + obj.h)*10))
+                    # y-cor checking
+                    yDiff = self.t.ycor() - obj.t.ycor()
+                    if yDiff < 100 and yDiff > 0:
+                        self.t.sety(obj.t.ycor()+((self.h + obj.h)*10))
 
-						# Setting velocity to 0
-						self.vec.y = 0
+                        # Setting velocity to 0
+                        self.vec.y = 0
 
-						# Breaking the function loop
-						return None
+                        # Breaking the function loop
+                        return None
 
-		# If nothing has happened
-		self.t.sety(self.t.ycor()-50)
-		self.vec.y = 0
+        # If nothing has happened
+        self.t.sety(self.t.ycor()-50)
+        self.vec.y = 0
 
-	def binds(self, wn, binds):
-		wn.onkeypress(self.up, binds[0])
-		wn.onkeypress(self.down, binds[1])
-		wn.onkeypress(self.left, binds[2])
-		wn.onkeypress(self.right, binds[3])
+    def binds(self, wn, binds):
+        wn.onkeypress(self.up, binds[0])
+        wn.onkeypress(self.down, binds[1])
+        wn.onkeypress(self.left, binds[2])
+        wn.onkeypress(self.right, binds[3])
 
-	# For 'moving one tick'
-	def tick(self):
-		self._up(self.vec.y)
-		self._right(self.vec.x)
+    # For 'moving one tick'
+    def tick(self):
+        self._up(self.vec.y)
+        self._right(self.vec.x)
 
-	def collided(self, other):
-		if abs(self.t.xcor() - other.t.xcor()) > (self.w + other.w)*10:
-			return False
-		if abs(self.t.ycor() - other.t.ycor()) > (self.h + other.h)*10:
-			return False
+    def collided(self, other):
+        if abs(self.t.xcor() - other.t.xcor()) > (self.w + other.w)*10:
+            return False
+        if abs(self.t.ycor() - other.t.ycor()) > (self.h + other.h)*10:
+            return False
 
-		return True
+        return True
 
 # BELOW: Subclasses of GameObject
 
@@ -96,69 +96,69 @@ class GameObject():
 
 class Obstacle(GameObject):
 
-	def __init__(self, scale, direction, color, x, y):
-		if direction == 'x':
-			super().__init__('square', color, scale, 1, x, y)
-		elif direction == 'y':
-			super().__init__('square', color, 1, scale, x, y)
-		else:
-			raise ValueError(f"Sorry, direction can only be x or y but was {direction} instead.")
+    def __init__(self, scale, direction, color, x, y):
+        if direction == 'x':
+            super().__init__('square', color, scale, 1, x, y)
+        elif direction == 'y':
+            super().__init__('square', color, 1, scale, x, y)
+        else:
+            raise ValueError(f"Sorry, direction can only be x or y but was {direction} instead.")
 
-		self.gravity = False
+        self.gravity = False
 
 
-	def check_for_collisions(self):
-		for obj in objs:
-			if obj != self:
-				if self.collided(obj):
-					obj._down(obj.vec.y)
+    def check_for_collisions(self):
+        for obj in objs:
+            if obj != self:
+                if self.collided(obj):
+                    obj._down(obj.vec.y)
 
-					# Bounces back with -0.6 velocity
-					obj.vec.bounce_y(0.6)
+                    # Bounces back with -0.6 velocity
+                    obj.vec.bounce_y(0.6)
 
 
 class Projectile(GameObject):
 
-	def __init__(self, direction, parent):
-		super().__init__('circle', '#000000', 0.3, 0.3, parent.t.xcor(), parent.t.ycor())
+    def __init__(self, direction, parent):
+        super().__init__('circle', '#000000', 0.3, 0.3, parent.t.xcor(), parent.t.ycor())
 
-		self.parent = parent
+        self.parent = parent
 
-	def destroy(self):
-		del self.t
+    def destroy(self):
+        del self.t
 
-	def check_coll(self, playersList):
-		for obj in objs:
-			if self.collided(obj):
+    def check_coll(self, playersList):
+        for obj in objs:
+            if self.collided(obj):
 
-				# Three cases - player, other projectile, or obstacle
-				
-				if obj in playersList:
-					if obj != self.parent:
-						obj.lives -= 1
+                # Three cases - player, other projectile, or obstacle
 
-					self.destroy()
+                if obj in playersList:
+                    if obj != self.parent:
+                        obj.lives -= 1
 
-				elif isinstance(obj, Obstacle):
+                    self.destroy()
 
-					# Replacing the obstacle
-					# But it can't collide with a player!
-					while True:
-						obj.goto(choice(range(-400, 450, 50)), choice(-300, 300, 50))
+                elif isinstance(obj, Obstacle):
 
-						c = False
-						for player in playersList:
-							if player.collided(obj):
-								c = True
+                    # Replacing the obstacle
+                    # But it can't collide with a player!
+                    while True:
+                        obj.goto(choice(range(-400, 450, 50)), choice(-300, 300, 50))
 
-						if not c:
-							break;
+                        c = False
+                        for player in playersList:
+                            if player.collided(obj):
+                                c = True
 
-					self.destroy()
+                        if not c:
+                            break;
 
-				elif isinstance(obj, Projectile):
+                    self.destroy()
 
-					obj.vec.bounce_x()
-					obj.vec.bounce_y()
-					self.vec.bounce_x()
-					self.vec.bounce_y()
+                elif isinstance(obj, Projectile):
+
+                    obj.vec.bounce_x()
+                    obj.vec.bounce_y()
+                    self.vec.bounce_x()
+                    self.vec.bounce_y()
--- movement_tools.py
+++ movement_tools.py
@@ -6,39 +6,39 @@ from math import sin, cos, atan, pi
 # A vector represents a direction of movement
 class Vector():
 
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-	def flip(self):
-		self.x *= -1
-		self.y *= -1
+    def flip(self):
+        self.x *= -1
+        self.y *= -1
 
-	# Rotate stuff
-	# rotate the vector in radians!
-	def rotate(self, factor):
-		newX = self.x*cos(factor) - self.y*sin(factor)
-		newY = self.x*sin(factor) + self.y*cos(factor)
+    # Rotate stuff
+    # rotate the vector in radians!
+    def rotate(self, factor):
+        newX = self.x*cos(factor) - self.y*sin(factor)
+        newY = self.x*sin(factor) + self.y*cos(factor)
 
-		self.x = newX
-		self.y = newY
+        self.x = newX
+        self.y = newY
 
-	def get_angle(self):
+    def get_angle(self):
 
-		try:
-			return atan(self.y/self.x)
-		except ZeroDivisionError:
-			return pi/2
+        try:
+            return atan(self.y/self.x)
+        except ZeroDivisionError:
+            return pi/2
 
-	# Again - radians!
-	def set_angle(self, angle):
-		self.rotate(angle - self.get_angle())
+    # Again - radians!
+    def set_angle(self, angle):
+        self.rotate(angle - self.get_angle())
 
-	def affect_gravity(self, factor, framerate):
-		self.y -= factor/framerate
+    def affect_gravity(self, factor, framerate):
+        self.y -= factor/framerate
 
-	def bounce_x(self, factor):
-		self.x *= -factor
+    def bounce_x(self, factor):
+        self.x *= -factor
 
-	def bounce_y(self, factor):
-		self.y *= -factor
+    def bounce_y(self, factor):
+        self.y *= -factor
